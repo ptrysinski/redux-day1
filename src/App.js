984e94c8ec62c77a2684e05a82bb79506dc9c4d0
Piotr Trysiński <ptrysinski@gmail.com>
2019-04-15 14:27:51 +0200

    preparing & sending messages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
 
-import {sendMessage} from './store'
+import './store'
+import { sendActionCreator } from './messages'
+import { store } from './store';
 
 class App extends Component {
+  state = {
+    messages: []
+  }
+
+  prepareMessage = () => {
+    const receiver = prompt('wpisz odbiorcę')
+    const text = prompt('wpisz treść wiadomości')
+
+    const message = sendActionCreator(receiver, text)
+
+    this.setState({
+      messages: this.state.messages.concat(message)
+    })
+  }
+
+  sendMessage = () => {
+    this.state.messages.forEach(
+      message => store.dispatch(message)
+    )
+
+    this.setState({ messages: [] })
+  }
+
   render() {
     return (
       <div>
         <button
-        onClick={() => sendMessage(prompt('receiver'), prompt('text'))}
+          onClick={this.prepareMessage}
+        >
+          Prepare message
+        </button>
+        <button
+          onClick={this.sendMessage}
         >
           Send message
         </button>
+        <div>
+          {
+            this.state.messages.map(
+              (message, i) => (
+                <div
+                  key={i + message.text}
+                >
+                  {message.receiver} |
+                  {message.text}
+                </div>
+              )
+            )
+          }
+        </div>
       </div>
     );
   }
